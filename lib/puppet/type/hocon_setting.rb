9033a97a95847d7d07d8ee0c6df7cdebb7a1aa8c
Chris Tessmer <chris.tessmer@onyxpoint.com>
2022-08-09 13:47:55 -0400

    Prep for migration to voxpup

--- lib/puppet/type/hocon_setting.rb
+++ lib/puppet/type/hocon_setting.rb
@@ -5,11 +5,35 @@ Puppet::Type.newtype(:hocon_setting) do
   end
 
   newparam(:setting, namevar: true) do
-    desc 'The name of the setting to be defined.'
+    desc <<~DESC
+      The name of the HOCON file setting to be defined.
+
+      This can be a top-level setting or a setting nested within another
+      setting.
+
+      To define a nested setting, give the full path to that setting
+      with each level separated by a `.` So, to define a setting `foosetting`
+      nested within a setting called `foo` contained on the top level, the
+      `setting` parameter would be set to `foo.foosetting`.
+
+      This parameter, along with `path`, is one of two namevars for the
+      `hocon_setting` type, meaning that Puppet will give an error if two
+      `hocon_setting` resources have the same `setting` and `path` parameters.
+
+      If no `setting` value is explicitly set, the title of the resource will
+      be used as the value of `setting`.
+    DESC
   end
 
   newparam(:path, namevar: true) do
-    desc 'The file Puppet will ensure contains the specified setting.'
+    desc <<~DESC
+      The HOCON file in which Puppet will ensure the specified setting.
+
+      This parameter, along with `setting`, is one of two namevars for the
+      `hocon_setting` type, meaning that Puppet will give an error if two
+      `hocon_setting` resources have the same `setting` and `path` parameters.
+    DESC
+
     validate do |value|
       unless (Puppet.features.posix? && value =~ %r{^\/}) || (Puppet.features.microsoft_windows? && (value =~ %r{^.:\/} || value =~ %r{^\/\/[^\/]+\/[^\/]+}))
         raise(Puppet::Error, "File paths must be fully qualified, not '#{value}'")
@@ -18,13 +42,119 @@ Puppet::Type.newtype(:hocon_setting) do
   end
 
   newproperty(:type) do
-    desc 'The value type'
+    desc <<~DESC
+      The type of the value passed into the `value` parameter.
+
+      This value should be a string, with valid values being `'number'`,
+      `'boolean'`, `'string'`, `'hash'`, `'array'`, `'array_element'`, and
+      `'text'`.
+
+      This parameter will not be need to be set most of the time, as the module
+      is generally smart enough to figure this out on its own. There are only
+      three cases in which this parameter is required.
+
+      The first is the case in which the `value` type is a single-element
+      array. In that case, the `type` parameter will need to be set to
+      `'array'`. So, for example, to add a single-element array, you would add
+      the following to your manifest:
+
+      ```
+      hocon_setting { 'single array setting':
+        ensure => present,
+        path => '/tmp/foo.conf',
+        setting => 'foo',
+        value => [1],
+        type => 'array',
+      }
+      ```
+
+      If you are trying to manage single entries in an array (for example,
+      adding to an array from a define) you will need to set the `'type'`
+      parameter to `'array_element'`. For example, to add to an existing array
+      in the 'foo' setting, you can add the following to your manifest:
+
+      ```
+      hocon_setting { 'add to array':
+        ensure  => present,
+        path    => '/tmp/foo.conf',
+        setting => 'foo',
+        value   => 2,
+        type    => 'array_element',
+      }
+      ```
+
+      Note: When adding an item via 'array_element', the array must already exist in the HOCON file.
+
+      Since this type represents a setting in a configuration file, you can
+      pass a string containing the exact text of the value as you want it to
+      appear in the file (this is useful, for example, if you want to set a
+      parameter to a map or an array but want comments or specific indentation
+      on elements in the map/array). In this case, `value` must be a string
+      with no leading or trailing whitespace, newlines, or comments that
+      contains a valid HOCON value, and the `type` parameter must be set to
+      `'text'`. This is an advanced use case, and will not be necessary for
+      most users.
+
+      So, for example, say you want to add a map with particular
+      indentation/comments into your configuration file at path `foo.bar`. You
+      could create a variable like so:
+
+      ```
+      $map =
+      "{
+          # This is setting a
+          a : b
+          # This is setting c
+              c : d
+        }"
+      ```
+
+      And your configuration file looks like so
+
+      ```
+      baz : qux
+      foo : {
+        a : b
+      }
+      ```
+
+      You could then write the following in your manifest
+
+      ```
+      hocon_setting { 'exact text setting':
+        ensure => present,
+        path => '/tmp/foo.conf',
+        setting => 'foo.bar',
+        value => $map,
+        type => 'text',
+      }
+      ```
+
+      And the resulting configuration file would look like so
+
+      ```
+      baz : qux
+      foo : {
+        a : b
+        bar : {
+            # This is setting a
+            a : b
+            # This is setting c
+                c : d
+        }
+      }
+      ```
+
+      Aside from these three cases, the `type` parameter does not need to be set.
+    DESC
     # This property has no default. If it is not supplied, the validation of the "value"
     # property will set one automatically.
   end
 
   newproperty(:value, array_matching: :all) do
-    desc 'The value of the setting to be defined.'
+    desc <<~DESC
+      The value of the HOCON file setting to be defined
+    DESC
 
     validate do |_val|
       # Grab the value we are going to validate
