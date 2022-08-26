58b8ec0d6fa0674a29dd14cf32f35e73191c1a46
Chris Tessmer <chris.tessmer@onyxpoint.com>
2022-08-26 16:48:19 -0400

    One puppet install is enough

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -10,19 +10,6 @@ require 'beaker/puppet_install_helper'
 
 unless ENV['RS_PROVISION'] == 'no'
   run_puppet_install_helper
-  hosts.each do |host|
-    puppet_version = (on default, puppet('--version')).output.chomp
-
-    puts "::notice ::puppet_version variable contents: '#{puppet_version}'"
-    if puppet_version =~ %r{Puppet Enterprise }
-      on host, puppet('module install puppetlabs-pe_gem')
-      on host, puppet('resource package hocon ensure=latest provider=pe_gem')
-    elsif ENV['PUPPET_INSTALL_TYPE'] != 'foss' && Gem::Version.new(puppet_version) >= Gem::Version.new('4.0.0')
-      on host, puppet('resource package hocon ensure=latest provider=puppet_gem')
-    else
-      on host, puppet('resource package hocon ensure=latest provider=gem')
-    end
-  end
 end
 
 RSpec.configure do |c|
